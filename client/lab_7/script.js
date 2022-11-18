53cb638fa83ae7bc710d51465d756e8dedc2535d
Soltis007 <112664532+Soltis007@users.noreply.github.com>
2022-11-17 21:00:39 -0500

    e

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -28,6 +28,7 @@ function injectHTML(list) {
     el.innerText = item.name;
     listEl.appendChild(el);
   });
+
   /*
     ## JS and HTML Injection
       There are a bunch of methods to inject text or HTML into a document using JS
@@ -52,27 +53,15 @@ function processRestaurants(list) {
     return list[index];
   });
   return newArray;
-  /*
-      ## Process Data Separately From Injecting It
-        This function should accept your 1,000 records
-        then select 15 random records
-        and return an object containing only the restaurant's name, category, and geocoded location
-        So we can inject them using the HTML injection function
+  function filterList(array, filterInputValue) {
+    return array.filter((array) => {
+      const lowerCaseName = item.name.toLowerCase();
+      const lowerCaseQuery = item.name.toLowerCase();
+      return lowerCaseName.includes(lowerCaseQuery);
+    });
+  }
 
-        You can find the column names by carefully looking at your single returned record
-        https://data.princegeorgescountymd.gov/Health/Food-Inspection/umjn-t2iz
-
-      ## What to do in this function:
-
-      - Create an array of 15 empty elements (there are a lot of fun ways to do this, and also very basic ways)
-      - using a .map function on that range,
-      - Make a list of 15 random restaurants from your list of 100 from your data request
-      - Return only their name, category, and location
-      - Return the new list of 15 restaurants so we can work on it separately in the HTML injector
-    */
-}
-
-async function mainEvent() {
+  async function mainEvent() {
   /*
       ## Main Event
         Separating your main programming from your side functions will help you organize your thoughts
@@ -80,66 +69,58 @@ async function mainEvent() {
         If you separate your work, when one piece is complete, you can save it and trust it
     */
 
-  // the async keyword means we can make API requests
-  const form = document.querySelector('.main_form'); // get your main form so you can do JS with it
-  const submit = document.querySelector('#get-resto'); // get a reference to your submit button
-  const loadAnimation = document.querySelector('.lds-ellipsis');
-  submit.style.display = 'none'; // let your submit button disappear
+    // the async keyword means we can make API requests
+    const form = document.querySelector('.main_form'); // get your main form so you can do JS with it
+    const submit = document.querySelector('#get-resto'); // get a reference to your submit button
+    const loadAnimation = document.querySelector('.lds-ellipsis');
+    submit.style.display = 'none'; // let your submit button disappear
 
-  /*
+    /*
       Let's get some data from the API - it will take a second or two to load
       This next line goes to the request for 'GET' in the file at /server/routes/foodServiceRoutes.js
       It's at about line 27 - go have a look and see what we're retrieving and sending back.
      */
-  const results = await fetch('/api/foodServicePG');
-  const arrayFromJson = await results.json(); // here is where we get the data from our request as JSON
+    const results = await fetch('/api/foodServicePG');
+    const arrayFromJson = await results.json(); // here is where we get the data from our request as JSON
 
-  /*
+    /*
       Below this comment, we log out a table of all the results using "dot notation"
       An alternate notation would be "bracket notation" - arrayFromJson["data"]
       Dot notation is preferred in JS unless you have a good reason to use brackets
       The 'data' key, which we set at line 38 in foodServiceRoutes.js, contains all 1,000 records we need
     */
-  console.table(arrayFromJson.data);
+    console.table(arrayFromJson.data);
 
-  // in your browser console, try expanding this object to see what fields are available to work with
-  // for example: arrayFromJson.data[0].name, etc
-  console.log(arrayFromJson.data[0]);
+    // in your browser console, try expanding this object to see what fields are available to work with
+    // for example: arrayFromJson.data[0].name, etc
+    console.log(arrayFromJson.data[0]);
 
-  // this is called "string interpolation" and is how we build large text blocks with variables
-  console.log(
-    `${arrayFromJson.data[0].name} ${arrayFromJson.data[0].category}`
-  );
+    // this is called "string interpolation" and is how we build large text blocks with variables
+    console.log(`${arrayFromJson.data[0].name} ${arrayFromJson.data[0].category}`);
 
-  // This IF statement ensures we can't do anything if we don't have information yet
-  if (arrayFromJson.data?.length > 0) {
-    // the question mark in this means "if this is set at all"
-    submit.style.display = 'block'; // let's turn the submit button back on by setting it to display as a block when we have data available
+    // This IF statement ensures we can't do anything if we don't have information yet
+    if (arrayFromJson.data?.length > 0) {
+      submit.style.display = 'block'; // let's turn the submit button back on by setting it to display as a block when we have data available
 
-    loadAnimation.classList.remove('lds-ellipsis');
-    loadAnimation.classList.add('lds-ellipsis-hidden');
+      loadAnimation.classList.remove('lds-ellipsis');
+      loadAnimation.classList.add('lds-ellipsis-hidden');
+
+      let currentList = [];
+
+      form.addEventListener('input', (event) => {
+        console.log(event.target.value);
+        currentList = filterList(currentList, event.target.value);
+        injectHTML(currentList);
+      });
+    }
     // And here's an eventListener! It's listening for a "submit" button specifically being clicked
     // this is a synchronous event event, because we already did our async request above, and waited for it to resolve
-    form.addEventListener('submit', (submitEvent) => {
-      // This is needed to stop our page from changing to a new URL even though it heard a GET request
-      submitEvent.preventDefault();
-
-      // This constant will have the value of your 15-restaurant collection when it processes
-      const restaurantList = processRestaurants(arrayFromJson.data);
-      console.log('restaurantList');
-      // And this function call will perform the "side effect" of injecting the HTML list for you
-      injectHTML(restaurantList);
-
-      // By separating the functions, we open the possibility of regenerating the list
-      // without having to retrieve fresh data every time
-      // We also have access to some form values, so we could filter the list based on name
+    form.addEventListener('submit', (submitEvent) => { // async has to be declared all the way to get an await
+      submitEvent.preventDefault(); // This prevents your page from refreshing!
+      currentList = processRestaurants(arrayFromJson.data);
+      injectHTML(currentList);
     });
   }
 }
 
-/*
-    This last line actually runs first!
-    It's calling the 'mainEvent' function at line 57
-    It runs first because the listener is set to when your HTML content has loaded
-  */
 document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
